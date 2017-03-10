aba139558855bb5c7309e2f96b6f6985f33d8aad
andygulin <andygulin@hotmail.com>
2017-03-10 11:47:40 +0800

    版本升级，代码更新...

--- src/main/java/spark/framework/sample/Application.java
+++ src/main/java/spark/framework/sample/Application.java
@@ -4,32 +4,54 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import spark.Spark;
 import spark.framework.sample.bean.User;
 import spark.framework.sample.service.UserService;
 
+import static spark.Spark.*;
+
 public class Application {
     public static void main(String[] args) {
-        Spark.port(8080);
+        port(8080);
 
         Injector injector = Guice.createInjector();
         UserService userService = injector.getInstance(UserService.class);
 
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").serializeNulls().create();
 
-        Spark.get("/user/get/:id", (request, response) ->
-                userService.getUser(Integer.parseInt(request.params(":id"))), gson::toJson);
+        path("/user", () -> {
+            get("/get/:id", (req, res) -> {
+                        res.type("application/json");
+                        res.header("Content-Encoding", "gzip");
+                        return userService.getUser(Integer.parseInt(req.params(":id")));
+                    }
+                    , gson::toJson);
 
-        Spark.get("/user/all", (request, response) ->
-                userService.getAll(), gson::toJson);
+            get("/all", (req, res) -> {
+                        res.type("application/json");
+                        res.header("Content-Encoding", "gzip");
+                        return userService.getAll();
+                    }
+                    , gson::toJson);
 
-        Spark.put("/user/insert", (request, response) ->
-                userService.save(gson.fromJson(request.body(), User.class)), gson::toJson);
+            put("/insert", (req, res) -> {
+                        res.type("application/json");
+                        res.header("Content-Encoding", "gzip");
+                        return userService.save(gson.fromJson(req.body(), User.class));
+                    }
+                    , gson::toJson);
 
-        Spark.delete("/user/delete/:id", (request, response) ->
-                userService.delete(Integer.parseInt(request.params(":id"))), gson::toJson);
+            delete("/delete/:id", (req, res) -> {
+                        res.type("application/json");
+                        res.header("Content-Encoding", "gzip");
+                        return userService.delete(Integer.parseInt(req.params(":id")));
+                    }
+                    , gson::toJson);
 
-        Spark.put("/user/update", (request, response) ->
-                userService.update(gson.fromJson(request.body(), User.class)), gson::toJson);
+            put("/update", (req, res) -> {
+                res.type("application/json");
+                res.header("Content-Encoding", "gzip");
+                return userService.update(gson.fromJson(req.body(), User.class));
+            }, gson::toJson);
+        });
     }
 }
